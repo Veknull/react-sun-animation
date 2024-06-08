aa4faf1044627e7f432aa48716ef302d37aa12e3
mdiarra <mdiarra@et.esiea.fr>
2024-06-08 18:02:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sun from './components/Sun';
+
+function App() {
+  return (
+    <div className="App">
+      <Sun />
+    </div>
+  );
+}
+
+export default App;
